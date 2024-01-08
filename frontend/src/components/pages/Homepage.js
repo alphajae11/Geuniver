b6e98be6d692944e4455a6080af2b186521fa1c0
alphajae11 <leejaegeun20#gmail.com>
2024-01-08 18:47:19 +0800

    Adding tailwindcss design to Phase 1

--- frontend/src/components/pages/Homepage.js
+++ frontend/src/components/pages/Homepage.js
@@ -3,61 +3,60 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../../styles/Homepage.css';
 
-var loggedIn = false;
+const loggedIn = false;
 
 const HomePage = () => {
   return (
     <>
-        {loggedIn ? (
-                <>
-                    <div className='Top_header'>
-                    <img src='../../styles/logos/Geuniver.png' alt="Geuniver_logo" width={100} height={100} />
-                    <div className='profile_options'>
-                        
-                        <Link to="/profile" className='ProfileButton'>
-                            <button>
-                                Check your profile
-                            </button>
-                        </Link>
-                    </div>
-                    </div>
-                    <div className='intro'>
-                        <h1>Welcome to Geuniver</h1>
-                        <p>
-                            Let's Try out the software products!
-                        </p>
-                    </div>
-                </>
-            ) : (
-                <>
-                    <div className='Top_header'>
-                    <img src='frontend/src/style/logos/Geuniver.png' alt="Geuniver_logo" width={50} height={50} />
-                    <div className='login_register_options'>
-                        <Link to="/register" className='RegisterButton'>
-                            <button>
-                                Register Here
-                            </button>
-                        </Link>
-                        <Link to='/login' className='LoginButton'>
-                            <button>
-                                Login Here
-                            </button>
-                        </Link>
-                    </div>
-                    </div>
-                    <div className='intro'>
-                        <h1>Welcome to Geuniver, User!</h1>
-                        <p>
-                            In Geuniver, you can try out all the software products created by JG. {'\n'}
-                            We will be making all features ranging from AI, games and so on.
-                        </p>
-                    </div>
-                </>
-            )
-        }
-        
+      {loggedIn ? (
+        <>
+          <div className='Top_header flex items-center justify-center'>
+            <img src='../../styles/logos/Geuniver.png' alt="Geuniver_logo" className='w-16 h-16 mb-4' />
+            <div className='profile_options ml-4'>
+              <Link to="/profile" className='ProfileButton ml-4'>
+                <button className='bg-blue-500 text-white px-4 py-2'>
+                  Check your profile
+                </button>
+              </Link>
+            </div>
+          </div>
+          <div className='intro text-center ml-4'>
+            <h1 className='text-4xl'>Welcome to Geuniver</h1>
+            <p className='text-lg whitespace-pre-line'>
+              Let's Try out the software products!
+            </p>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className='Top_header flex items-center justify-center'>
+            <img src='../../styles/logos/Geuniver.png' alt="Geuniver_logo" className='w-10 h-10 mb-4' />
+            <div className='login_register_options ml-10'>
+              <Link to="/register" className='RegisterButton mb-2'>
+                <button className='bg-green-500 text-white px-4 py-2 block w-full'>
+                  Register Here
+                </button>
+              </Link>
+              <Link to='/login' className='LoginButton'>
+                <button className='bg-blue-500 text-white px-4 py-2 block w-full mt-2'>
+                  Login Here
+                </button>
+              </Link>
+            </div>
+          </div>
+          <div className='intro text-center'>
+            <h1 className='text-4xl'>Welcome to Geuniver, User!</h1>
+            <p className='text-lg'>
+              In Geuniver, you can try out all the software products created by JG.
+              <br />
+              We will be making all features ranging from AI, games and so on.
+            </p>
+          </div>
+        </>
+      )}
     </>
   );
 };
 
 export default HomePage;
+
